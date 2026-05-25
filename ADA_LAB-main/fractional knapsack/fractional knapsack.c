5421e5deee69befb60beec88edcf0ac9b2afcb89
animeshyadavbms <animeshyadav.cs24@bmsce.ac.in>
2026-05-25 09:52:22 +0530

    Update fractional knapsack.c

--- ADA_LAB-main/fractional knapsack/fractional knapsack.c
+++ ADA_LAB-main/fractional knapsack/fractional knapsack.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
-// Structure to store item details
 struct Item {
     int weight;
     int value;
     float ratio;
 };
 
-// Function to swap two items
 void swap(struct Item *a, struct Item *b) {
     struct Item temp = *a;
     *a = *b;
     *b = temp;
 }
 
-// Sort items by value/weight ratio in descending order
 void sortItems(struct Item items[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
@@ -25,7 +22,6 @@ void sortItems(struct Item items[], int n) {
     }
 }
 
-// Fractional Knapsack function
 float fractionalKnapsack(int capacity, struct Item items[], int n) {
     sortItems(items, n);
 
@@ -33,12 +29,10 @@ float fractionalKnapsack(int capacity, struct Item items[], int n) {
 
     for (int i = 0; i < n; i++) {
 
-        // If whole item can be taken
         if (capacity >= items[i].weight) {
             capacity -= items[i].weight;
             totalValue += items[i].value;
         }
-        // Take fraction of item
         else {
             totalValue += items[i].value * ((float)capacity / items[i].weight);
             break;
@@ -56,12 +50,10 @@ int main() {
 
     struct Item items[n];
 
-    // Input values and weights
     for (int i = 0; i < n; i++) {
         printf("Enter value and weight of item %d: ", i + 1);
         scanf("%d %d", &items[i].value, &items[i].weight);
 
-        // Calculate ratio
         items[i].ratio = (float)items[i].value / items[i].weight;
     }
 
